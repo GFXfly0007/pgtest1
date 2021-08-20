0f737c65e2c2243019fe029b0c05cfa5d2b758a8
GFXfly0007 <15694493127@163.com>
2021-08-20 17:50:16 +0800

    create

---
+++ test/pytest.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+# @Time         : 2021/8/20 17:48
+# @Author       : GZ
+# @Email        : 15694493127@163.com
+# @File         : pytest.py
+# @Introduction :
+print("hello")
